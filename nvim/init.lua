8706d8348372cfff1155f25bc7236abfcc061826
nakulj <3213360+nakulj@users.noreply.github.com>
2022-11-30 17:50:17 -0800

    add vim fugitive

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,7 @@ packer.startup(function()
   use 'mhinz/vim-signify'
   use 'scrooloose/nerdcommenter'
   use 'tpope/vim-surround'
+  use 'tpope/vim-fugitive'
   use 'wbthomason/packer.nvim'
   use {'neoclide/coc.nvim', branch = 'release'}
   use {
