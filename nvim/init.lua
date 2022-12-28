3faeea46340ad778332b2301a7639c1512d1ec28
nakulj <3213360+nakulj@users.noreply.github.com>
2022-12-28 12:24:59 -0800

    remove default settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -75,12 +75,6 @@ vim.cmd[[colorscheme tokyonight-night]]
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = false,
   },
   sections = {
     lualine_a = {'mode'},
