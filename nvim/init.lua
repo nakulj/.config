93141b90f4a9e56088f986c4b9986f1e6abde998
nakulj <3213360+nakulj@users.noreply.github.com>
2023-01-09 13:38:03 -0800

    remove default config

--- nvim/init.lua
+++ nvim/init.lua
@@ -75,34 +75,9 @@ require("telescope").setup{
 vim.cmd[[colorscheme tokyonight-night]]
 
 require('lualine').setup {
-  options = {
-    icons_enabled = false,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
   tabline = {
     lualine_a = {'buffers'},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
   },
-  extensions = {}
 }
 
 local noremap = function (modes, left, right)
