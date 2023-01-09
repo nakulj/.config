d39a437398fbb671ace7be4ec39dac43a6680cce
nakulj <3213360+nakulj@users.noreply.github.com>
2023-01-09 13:03:44 -0800

    telescope fallback

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,7 +117,18 @@ noremap('nv',';',':')
 noremap('nv','<leader>f',':FormatLines<CR>')
 
 
-noremap('n','<C-p>',':Telescope git_files<CR>')
+_G.project_files = function()
+  local utils = require('telescope.utils')
+  local builtin = require('telescope.builtin')
+  local _, ret, _ = utils.get_os_command_output({'git', 'rev-parse', '--is-inside-work-tree'})
+  if ret == 0 then
+      builtin.git_files()
+  else
+      builtin.find_files()
+  end
+end
+
+noremap('n','<C-p>','<cmd>lua project_files()<CR>')
 noremap('n','<leader>w',':bd<CR>')
 
 noremap('nv','<TAB>',':bn<CR>')
