c1443472733893d63318fe7b1407f5934a4c6304
nakulj <3213360+nakulj@users.noreply.github.com>
2023-01-13 15:34:40 -0800

    add thrift plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,6 +35,7 @@ packer.startup(function()
   use 'tpope/vim-surround'
   use 'tpope/vim-fugitive'
   use 'wbthomason/packer.nvim'
+  use 'solarnz/thrift.vim'
   use 'michaeljsmith/vim-indent-object'
   use {'neoclide/coc.nvim', branch = 'release'}
   use {
